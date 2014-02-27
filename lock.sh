6948a567c67f2dcf7b8621fcd94883712faf5f13
Axel Wagner <mail@merovius.de>
2014-02-27 23:02:34 +0100

    Gracefully handle symlinks

--- lock.sh
+++ lock.sh
@@ -3,5 +3,6 @@
 export TZ=UTZ+12
 H=`date +%H`
 M=$(( (`date +%M` / 15)*15 ))
-FILE=`printf $(dirname $0)/img/resized/%.2dh%.2dm.png $H $M`
+DIR=$(dirname `readlink -f $0`)
+FILE=`printf $DIR/img/resized/%.2dh%.2dm.png $H $M`
 i3lock -i $FILE
