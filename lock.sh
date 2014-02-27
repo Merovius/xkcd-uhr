2013a11280f4fb246f6526769f69add4c95a6b72
Johannes Visintini <github@joker234.de>
2014-02-27 20:53:53 +0100

    Using dirname instead of /home/mero

--- lock.sh
+++ lock.sh
@@ -3,5 +3,5 @@
 export TZ=UTZ+12
 H=`date +%H`
 M=$(( (`date +%M` / 15)*15 ))
-FILE=`printf /home/mero/src/xkcd-uhr/img/resized/%.2dh%.2dm.png $H $M`
+FILE=`printf $(dirname $0)/img/resized/%.2dh%.2dm.png $H $M`
 i3lock -i $FILE
